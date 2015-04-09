bcdae963f7d479830f1fd348457dd01f40431008
Balazs Szerencsi <balazs.szerencsi@icloud.com>
2015-04-09 07:53:11 +0200

    Initial commit.

---
+++ lib/sequel/pool_cleaner.rb
@@ -0,0 +1,7 @@
+require "sequel/pool_cleaner/version"
+
+module Sequel
+  module PoolCleaner
+    # Your code goes here...
+  end
+end
---
+++ lib/sequel/pool_cleaner/version.rb
@@ -0,0 +1,5 @@
+module Sequel
+  module PoolCleaner
+    VERSION = "0.1.0"
+  end
+end
